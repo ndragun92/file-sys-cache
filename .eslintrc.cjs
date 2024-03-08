8450b2ea449b9bfa50a0f22b22958b2375d5edb0
ndragun92 <nemanjadragun92@gmail.com>
2024-03-08 22:57:17 +0100

    chore: exclude coverage from lint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,7 +3,7 @@ module.exports = {
     browser: true,
     es2021: true
   },
-  ignorePatterns: ['dist/', 'node_modules/', 'src/**.spec.ts'],
+  ignorePatterns: ['dist/', 'node_modules/', 'src/**.spec.ts', 'coverage/'],
   extends: 'standard-with-typescript',
   overrides: [
     {
