134b60365076b25a201af7245fcf9e07d2f83fec
ndragun92 <nemanjadragun92@gmail.com>
2024-03-12 01:22:44 +0100

    feat: Add new files and updates for website components
    This commit introduces several new files and updates pertaining to the website's components. It includes changes to the "_nuxt" folder which contains the built production-ready version of the application. Specific additions comprise new JSON configurations, CSS styling, and JavaScript functional components. These contribute to enhancing the website's visuals and functionality such as alerts, badges, and various interactive elements.

---
+++ docs/_nuxt/EditOnLink.C_MtJmQF.js
@@ -0,0 +1,1 @@
+import{_ as m}from"./EditOnLink.vue.DrMSWnMS.js";import"./entry.D6gWEDxd.js";export{m as default};
---
+++ docs/_nuxt/ProseTr.boiqnRgl.js
@@ -0,0 +1,1 @@
+import{l as t,b as n,c as a,Z as o}from"./entry.D6gWEDxd.js";const r={};function c(e,s){return n(),a("tr",null,[o(e.$slots,"default",{},void 0,!0)])}const l=t(r,[["render",c],["__scopeId","data-v-759aaa1b"]]);export{l as default};
